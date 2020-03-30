84da1f40e8105ef5c07fc57916d5ff7ade21d8b3
Abdulaziz Erel <azizerel69@gmail.com>
2020-03-30 20:41:09 +0300

    Google,github,facebook api constant added

---
+++ src/constants/apiConstants.js
@@ -0,0 +1,5 @@
+export const GOOGLE_CLIENT_ID       = "YOUR_GOOGLE_CLIENT_ID";
+
+export const FACEBOOK_APP_ID       = "YOUR_FACEBOOK_APP_ID";
+
+export const GITHUB_CLIENT_ID       = "YOUR_GITHUB_CLIENT_ID";
