f57db1e13f97d164d7b7674e53c028978ae162fd
Abdulaziz Erel <azizerel69@gmail.com>
2020-03-30 22:32:47 +0300

    removed unused imports

--- src/panels/rightPanel.js
+++ src/panels/rightPanel.js
@@ -3,21 +3,12 @@ import TextField                          from '@material-ui/core/TextField';
 import Checkbox                           from '@material-ui/core/Checkbox';
 import FormControlLabel                   from '@material-ui/core/FormControlLabel';
 import Button                             from '@material-ui/core/Button';
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import {GOOGLE_CLIENT_ID,FACEBOOK_APP_ID,
-        GITHUB_CLIENT_ID}                 from '../constants/apiConstants.js';
-import GoogleLogin                        from 'react-google-login';
-
+import ArrowForwardIcon                   from '@material-ui/icons/ArrowForward';
+import {FACEBOOK_APP_ID}                  from '../constants/apiConstants.js';
 import FacebookLogin                      from 'react-facebook-login';
-import GitHubLogin                        from 'react-github-login';
-
-import FacebookIcon from '@material-ui/icons/Facebook';
 
 import { makeStyles } from '@material-ui/core/styles';
 class RightPanel extends React.Component {
-
-
-
 
   render() {
 
@@ -25,37 +16,29 @@ class RightPanel extends React.Component {
       console.log(response);
     }
 
-    const responseGoogle = (response) => {
-      console.log(response);
-    }
-
-    const responseGithub = (response) => {
-      console.log(response);
-    }
-
     return (
       <div className="rightPanel">
 
-      <div className="div-center" style={{position: 'relative',display:'grid',width: '80%'}}>
+        <div className="div-center" style={{position: 'relative',display:'grid',width: '80%'}}>
 
-           <TextField label="Name" className="register-input"/>
+             <TextField label="Name" className="register-input"/>
 
-           <TextField label="Surname" className="register-input" />
+             <TextField label="Surname" className="register-input" />
 
-           <TextField label="Password" type="password" className="register-input"/>
+             <TextField label="Password" type="password" className="register-input"/>
 
-           <FormControlLabel className="register-checkbox" control={<Checkbox name="termCheck" color="primary"/>} label="I have read and accept Terms of Use and Privacy Policy"/>
+             <FormControlLabel className="register-checkbox" control={<Checkbox name="termCheck" color="primary"/>} label="I have read and accept Terms of Use and Privacy Policy"/>
 
-           <Button variant="contained" className="register-button" endIcon={<ArrowForwardIcon />} color="primary" >
-            Register
-          </Button>
+             <Button variant="contained" className="register-button" endIcon={<ArrowForwardIcon />} color="primary" >
+                Register
+             </Button>
 
-          <FacebookLogin
-            appId= {FACEBOOK_APP_ID}
-            fields="name,email,picture"
-            callback={responseFacebook}
-            cssClass="facebook-button div-center"
-            />
+            <FacebookLogin
+              appId= {FACEBOOK_APP_ID}
+              fields="name,email,picture"
+              callback={responseFacebook}
+              cssClass="facebook-button div-center"
+              />
           </div>
       </div>
     );
